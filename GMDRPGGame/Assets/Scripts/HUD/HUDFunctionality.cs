03fc71a719d2469b769364de206581d207a08f59
Dalius Seminauskas <285039@VIAUC.DK>
2022-04-02 17:22:36 +0200

    EXP, Mana, Health HUD bar implementation

---
+++ GMDRPGGame/Assets/Scripts/HUD/HUDFunctionality.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using RPG.Core;
+using TMPro;
+
+public class HUDFunctionality : MonoBehaviour
+{
+    private GameObject player;
+
+    private Slider healthSlider;
+    private Slider expSlider;
+    private Slider manaSlider;
+
+    private TextMeshProUGUI healthText;
+    private TextMeshProUGUI expText;
+    private TextMeshProUGUI manaText;
+
+    void Start(){
+        if (player == null)
+            player = GameObject.FindWithTag("Player");
+        
+        if (expSlider == null)
+            expSlider = GameObject.FindWithTag("EXPSlider").GetComponent<Slider>();
+
+        if (manaSlider == null)
+            manaSlider = GameObject.FindWithTag("ManaSlider").GetComponent<Slider>();
+
+        if (healthSlider == null)
+            healthSlider = GameObject.FindWithTag("HPSlider").GetComponent<Slider>();
+
+        if (healthText == null)
+            healthText = GameObject.FindWithTag("HPText").GetComponent<TextMeshProUGUI>();;
+
+        if (expText == null)
+            expText = GameObject.FindWithTag("EXPText").GetComponent<TextMeshProUGUI>();
+
+        if (manaText == null)
+            manaText = GameObject.FindWithTag("ManaText").GetComponent<TextMeshProUGUI>();
+
+        healthSlider.minValue = 0;
+        healthSlider.maxValue = player.GetComponent<Health>().getHealthPoints();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        updateHPTextAndSlider();
+    }
+
+    void updateHPTextAndSlider(){
+
+        healthSlider.GetComponent<Slider>().value = player.GetComponent<Health>().getHealthPoints();
+        healthText.text = player.GetComponent<Health>().getHealthPoints().ToString();
+    }
+}
